0c0a920753463eb6c53350045f6f849166e81ec8
ghtea <eiirwp@gmail.com>
2020-07-20 13:12:24 +0000

    for clone

---
+++ build/precache-manifest.85a5ffddf13932bf69cde24819e4e0c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5b9f86765efc8f0ee97a1492d3e050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0194ccb5fab0a22d3756",
+    "url": "/static/js/2.b13bbe1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b13bbe1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb1c7a8585a9ff51ee8",
+    "url": "/static/js/main.e47612a8.chunk.js"
+  },
+  {
+    "revision": "7423f9eb6b013e212d64",
+    "url": "/static/js/runtime-main.d9ea9ed6.js"
+  }
+]);
